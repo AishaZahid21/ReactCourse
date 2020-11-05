2ce797d6a312dfb973425a24e71b7bb92655fba6
Ayesha Zahid <47062178+AishaZahid21@users.noreply.github.com>
2020-11-05 22:21:08 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hi(){
+  return <div class="main">
+    <h1 class="header">Hello World</h1>
+    <br/>
+    <p class="ptag"><strong>Today's Tasks</strong></p>
+    <ol class="list">
+      <li>React practice</li>
+      <li>React practice</li>
+      <li>React practice</li>
+      <li>React practice</li>
+      <li>React practice</li>
+    </ol>
+<p>Just simple JavaScript 3 + 5 = {3+5}</p>
+  </div>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Hi/>,document.querySelector("#root"));
