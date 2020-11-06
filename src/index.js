bce9da212d9bb325c0c3b66133f82b284e374694
Ayesha Zahid <47062178+AishaZahid21@users.noreply.github.com>
2020-11-06 21:49:26 +0500

    add button in room component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDom from "react-dom"
 
 
 function Room(){
-  const [isLit , setLit] = useState(false);
-return <div>This room is {isLit ? 'lit' : 'dark'}</div>
+  const [isLit , setLit] = useState(true);
+return <div class="room">This room is {isLit ? 'lit' : 'dark'}
+<br/>
+<button onClick={()=>setLit(!isLit)}> Flip</button>
+
+</div>
 }
 
 ReactDom.render(<Room/>, document.getElementById("root"));
