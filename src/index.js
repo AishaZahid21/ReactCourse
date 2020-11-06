b74353cbed19316da410f96a019bfcc9130cafde
Ayesha Zahid <47062178+AishaZahid21@users.noreply.github.com>
2020-11-06 22:01:18 +0500

    add index.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React , {useState} from "react"
-import ReactDom from "react-dom"
-
+import React , {useState} from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
 function Room(){
   const [isLit , setLit] = useState(true);
-return <div class="room">This room is {isLit ? 'lit' : 'dark'}
+  const brightness = isLit ? 'lit': 'dark'
+return <div class={`room ${brightness}`}>This room is {isLit ? 'lit' : 'dark'}
 <br/>
-<button onClick={()=>setLit(!isLit)}> Flip</button>
+<button onClick={()=>setLit(!isLit)}> Flip </button>
 
 </div>
 }
