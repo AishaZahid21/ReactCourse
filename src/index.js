be486603f6e10580f3632e9b22d79bb321d3258e
Ayesha Zahid <47062178+AishaZahid21@users.noreply.github.com>
2020-11-06 23:33:49 +0500

    add temperature value

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import "./index.css";
 
 function Room(){
   const [isLit , setLit] = useState(true);
+  const [tempValue]=useState({day:72,night:22})
   const brightness = isLit ? 'lit': 'dark'
-return <div class={`room ${brightness}`}>This room is {isLit ? 'lit' : 'dark'}
+return <div className={`room ${brightness}`}>
+  This room is {isLit ? 'lit' : 'dark'}
 <br/>
-<button onClick={()=>setLit(!isLit)}> Flip </button>
+Room temperature is {isLit ? tempValue.day : tempValue.night}
+<br/>
+<button onClick={()=>{
+  if(!isLit){
+    setLit(!isLit)
+  }
+}}> ON </button>
+<button onClick={()=>{
+  if(isLit){
+    setLit(!isLit)
+  }
+}}> OFF </button>
+
 
 </div>
 }
